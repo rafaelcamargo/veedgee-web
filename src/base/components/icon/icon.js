cbabb2e5e26b7b35d6a8aeb2089eb271874fe2e4
Rafael Camargo <hello@rafaelcamargo.com>
2026-08-11 18:43:14 -0300

    feat: improve icon

--- src/base/components/icon/icon.js
+++ src/base/components/icon/icon.js
@@ -1,9 +1,25 @@
-export const Icon = ({ name, children, ...rest }) => {
+export const Icon = ({ name, children, strokeColor, strokeWidth, size, style, ...rest }) => {
+  const iconStyle = {
+    ...buildIconStyles({ strokeColor, strokeWidth, size }),
+    ...style
+  };
+
   return (
-    <div className={`v-icon v-icon-${name}`} aria-hidden="true" {...rest}>
+    <div className={`v-icon v-icon-${name}`} aria-hidden="true" style={iconStyle} {...rest}>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40">
         {children}
       </svg>
     </div>
   );
 };
+
+function buildIconStyles({ strokeColor, strokeWidth, size }) {
+  return Object.entries({
+    '--v-icon-stroke-color': strokeColor,
+    '--v-icon-stroke-width': strokeWidth,
+    '--v-icon-width': size,
+    '--v-icon-height': size
+  }).reduce((result, [key, value]) => {
+    return value ? { ...result, [key]: value } : result;
+  }, {});
+}
